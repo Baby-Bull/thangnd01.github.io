191995aac9e8101835e5290af279888890c9db06
babybull <abc@mm.com>
2021-02-28 23:58:34 +0700

    up

--- 1.js
+++ 1.js
@@ -1,4 +1,14 @@
-﻿ $(function(){
- 
-})  
+﻿$(document).ready(function() {
+	
+	$('.xuong').on('click',  function(event) {
+		event.preventDefault();
+		$('html,body').animate({scrollTop:800}, 1000);
+	});
+
+	$('.khoitt1 a').click(function(event) {
+		/* Act on the event */
+		console.log('test');
+		$(this).toggleClass('raxa');
+	});
+});
  
